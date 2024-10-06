17ceebb02acbcc3ef1e530546040a0515a1e02cc
GiovanniAlca <gra483120@gmail.com>
2024-10-06 17:17:04 -0500

    Se agrega el proyecto

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// index.js
+function toggleMenu() {
+    const menu = document.querySelector('.encabezado__menu');
+    menu.classList.toggle('encabezado__menu--activo'); // Alterna la clase para mostrar/ocultar el menú
+}
